167b35341e8ac3ddd95bce4c4d68fb3a6d8d95aa
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-02-01 23:19:14 +0300

    Refactor posts slice extra reducers

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -62,7 +62,7 @@ const postsSlice = createSlice({
       })
       .addCase(fetchPosts.fulfilled, (state, action) => {
         state.status = 'succeeded'
-        
+
         let min = 1
         const loadedPosts = action.payload.map(post => {
           post.date = sub(new Date(), { minutes: min++ }).toISOString()
@@ -78,6 +78,35 @@ const postsSlice = createSlice({
 
         // update fetched posts to the array
         state.posts = state.posts.concat(loadedPosts)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        // Fix for API post IDs:
+        // Creating sortedPosts & assigning the id 
+        // would be not be needed if the fake API 
+        // returned accurate new post IDs
+        const sortedPosts = state.posts.sort((a, b) => {
+          if (a.id > b.id) return 1
+          if (a.id < b.id) return -1
+          return 0
+        })
+        action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
+        // End fix for fake API post IDs 
+
+        action.payload.userId = Number(action.payload.userId)
+        action.payload.date = new Date().toISOString();
+        action.payload.reactions = {
+          thumbsUp: 0,
+          hooray: 0,
+          heart: 0,
+          rocket: 0,
+          eyes: 0
+        }
+        // console.log(action.payload)
+        state.posts.push(action.payload)
       })
   }
 })
