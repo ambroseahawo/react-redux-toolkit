812cbf13e8fe22d43bacbba73dc07733730c8621
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-02-08 23:56:16 +0300

    Select post by id

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -112,6 +112,7 @@ const postsSlice = createSlice({
 })
 
 export const selectAllPosts = (state) => state.posts.posts;
+export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId)
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
 
