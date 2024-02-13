0fc11df516cbd7cdc9e19aaf7f9b41819db50e99
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-02-13 23:53:52 +0300

    Implement Delete Post State

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -20,13 +20,24 @@ export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPos
   return response.data
 })
 
-export const updatePost = createAsyncThunk('posts/updatePost', async(initialPost) => {
+export const updatePost = createAsyncThunk('posts/updatePost', async (initialPost) => {
   const { id } = initialPost
   try {
     const response = await axios.put(`${POSTS_URL}/${id}`, initialPost)
     return response.data
-  }catch(err){
+  } catch (err) {
     console.log(err.message)
+  }
+})
+
+export const deletePost = createAsyncThunk('posts/deletePost', async (initialPost) => {
+  const { id } = initialPost;
+  try {
+    const response = await axios.delete(`${POSTS_URL}/${id}`)
+    if (response?.status === 200) return initialPost;
+    return `${response?.status}: ${response?.statusText}`;
+  } catch (err) {
+    return err.message;
   }
 })
 
@@ -119,7 +130,7 @@ const postsSlice = createSlice({
         state.posts.push(action.payload)
       })
       .addCase(updatePost.fulfilled, (state, action) => {
-        if(!action.payload?.id){
+        if (!action.payload?.id) {
           console.log('Update Could not Complete')
           console.log(action.payload)
           return
@@ -128,6 +139,16 @@ const postsSlice = createSlice({
         action.payload.date = new Date().toISOString()
         const posts = state.posts.filter(post => post.id !== id)
         state.posts = [...posts, action.payload]
+      })
+      .addCase(deletePost.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+          console.log('Delete could not complete')
+          console.log(action.payload)
+          return;
+        }
+        const { id } = action.payload;
+        const posts = state.posts.filter(post => post.id !== id);
+        state.posts = posts;
       })
   }
 })
